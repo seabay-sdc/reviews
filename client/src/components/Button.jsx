5fa9effd29770659e4e7df5e21de1faf081e3bf7
Miles Zander <mileszander@Miless-MacBook-Pro.local>
2019-07-05 19:58:48 -0500

    headers completed

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styling.css';
+
+
+const Button = () => (
+    <div>
+        <button id="button" type="button">Write Review</button>
+    </div>
+  );
+
+
+  export default Button 
