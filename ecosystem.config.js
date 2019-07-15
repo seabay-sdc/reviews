997e0681a6a27fad6ba4d208f780b94ce0121c5c
Miles Zander <mileszander@Miless-MacBook-Pro.local>
2019-07-15 17:21:20 -0500

    missing comma

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: "ec2-52-15-94-164.us-east-2.compute.amazonaws.com"
+        host: "ec2-52-15-94-164.us-east-2.compute.amazonaws.com",
         key: '~/.ssh/review.pem',
         ref: 'origin/master',
         repo: 'https://github.com/seabay-hratx42-fec/reviews.git',
