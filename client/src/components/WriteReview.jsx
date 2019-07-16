756b2f5b505a0a1a4ec95c8ffe61e266e6b31aaa
Miles Zander <mileszander@Miless-MacBook-Pro.local>
2019-07-16 15:19:22 -0500

    added ability to add reviews to item

--- client/src/components/WriteReview.jsx
+++ client/src/components/WriteReview.jsx
@@ -1,74 +1,94 @@
-// import React from 'react';
-// import './styling.css';
-// import Modal from 'react-modal';
+import React from 'react';
+import './styling.css';
+import Modal from 'react-modal';
 
-// const customStyles = {
-//     content : {
-//       top                   : '50%',
-//       left                  : '50%',
-//       right                 : 'auto',
-//       bottom                : 'auto',
-//       marginRight           : '-50%',
-//       transform             : 'translate(-50%, -50%)'
-//     }
-//   };
+const customStyles = {
+    content : {
+      top                   : '50%',
+      left                  : '50%',
+      right                 : 'auto',
+      bottom                : 'auto',
+      marginRight           : '-50%',
+      transform             : 'translate(-50%, -50%)',
+    }
+  };
    
-//   // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
-//   Modal.setAppElement('#item-reviews')
+  // Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+  Modal.setAppElement('#item-reviews')
    
-//   class WriteRev extends React.Component {
-//     constructor() {
-//       super();
+  class WriteRev extends React.Component {
+    constructor() {
+      super();
    
-//       this.state = {
-//         modalIsOpen: false
-//       };
+      this.state = {
+        modalIsOpen: false,
+        review: "",
+        user: "",
+        title: ""
+      };
    
-//       this.openModal = this.openModal.bind(this);
-//       this.afterOpenModal = this.afterOpenModal.bind(this);
-//       this.closeModal = this.closeModal.bind(this);
-//     }
+      this.openModal = this.openModal.bind(this);
+      this.afterOpenModal = this.afterOpenModal.bind(this);
+      this.closeModal = this.closeModal.bind(this);
+      this.handleReview = this.handleReview.bind(this);
+      this.handleUser = this.handleUser.bind(this);
+      this.handleTitle = this.handleTitle.bind(this);
+    }
    
-//     openModal() {
-//       this.setState({modalIsOpen: true});
-//     }
+    openModal() {
+      this.setState({modalIsOpen: true});
+    }
    
-//     afterOpenModal() {
-//       // references are now sync'd and can be accessed.
-//       this.subtitle.style.color = '#f00';
-//     }
+    afterOpenModal() {
+      // references are now sync'd and can be accessed.
+      this.subtitle.style.color = '#f00';
+    }
    
-//     closeModal() {
-//       this.setState({modalIsOpen: false});
-//     }
-   
-//     render() {
-//       return (
-//         <div>
-//           <button onClick={this.openModal}>Open Modal</button>
-//           <Modal
-//             isOpen={this.state.modalIsOpen}
-//             onAfterOpen={this.afterOpenModal}
-//             onRequestClose={this.closeModal}
-//             style={customStyles}
-//             contentLabel="Example Modal"
-//           >
-   
-//             <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-//             <button onClick={this.closeModal}>close</button>
-//             <div>I am a modal</div>
-//             <form>
-//               <input />
-//               <button>tab navigation</button>
-//               <button>stays</button>
-//               <button>inside</button>
-//               <button>the modal</button>
-//             </form>
-//           </Modal>
-//         </div>
-//       );
-//     }
-//   }
+    closeModal() {
+      this.setState({modalIsOpen: false});
+    }
+
+    handleReview(event) {
+        this.setState({review: event.target.value});
+      }
+
+    handleUser(event) {
+        this.setState({user: event.target.value});
+    }
+
+    handleTitle(event) {
+        this.setState({title: event.target.value});
+    }
+
+    render() {
+      return (
+        <div id="buttonTile">
+          <button id="buttonR"onClick={this.openModal}>Write Review</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+            size="lg"
+          >
+            <div ref={subtitle => this.subtitle = subtitle}></div>
+            <center><h2 id="newReviewTitle" >Write Review</h2></center>
+            <br></br>
+            <form onSubmit={this.props.handleSubmit(this.state.review, this.state.user, this.state.title)}>
+              <input id="userName" placeholder="User Name" value={this.state.user} onChange={this.handleUser} />
+              <br></br>
+              <input id="userName" placeholder="Title" value={this.state.title} onChange={this.handleTitle} />
+            <br></br>
+              <input id="reviewForm" placeholder="Review..." value={this.state.review} onChange={this.handleReview} />
+            <br></br>
+            <center><input type="submit" value="Submit" /></center>
+            </form>
+          </Modal>
+        </div>
+      );
+    }
+  }
   
 
-//   export default WriteRev 
+  export default WriteRev 
