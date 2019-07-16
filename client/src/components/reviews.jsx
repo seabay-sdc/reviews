29ed5e418dfaeccab008cef66595f869f4f1e95f
Miles Zander <mileszander@Miless-MacBook-Pro.local>
2019-07-16 11:45:57 -0500

    event listener now triggers new get request

--- client/src/components/reviews.jsx
+++ client/src/components/reviews.jsx
@@ -5,17 +5,17 @@ import './styling.css';
 const Reviews = (props) => {
     return (
     <div key="ab" className="reviewContainerR">
-        {props.list.map(review => {
+        {props.list.map((review,i) => {
             return (
             <>
-            <div key={"a" + review.id} className="userR">
+            <div className="userR">
                 <br></br>
                 <font > by: </font>
                 <a href="" className="linksR">{review.name}</a>
                 <br></br>
                 <font color="grey">{review.date} </font>
             </div>
-            <div key={"b" + review.id} className="reviewR">
+            <div className="reviewR">
                 <div className="titleR"> {review.title} </div>
                 <div>{review.review} </div>
             </div>
