a2878946e3f87e26527e27a932089031c69ad074
Miles Zander <mileszander@Miless-MacBook-Pro.local>
2019-07-15 19:46:00 -0500

    event listener:

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -13,6 +13,7 @@ class App extends React.Component {
       id: 5,
       reviews: []
     }
+    this.getItem = this.getItem.bind(this)
   }
 
   componentDidMount() {
@@ -24,38 +25,22 @@ class App extends React.Component {
       .catch(function (error) {
       console.log(error)
     })
-    // document.addEventListener('testEvent', data => {
-    //   console.log('Item view received this id: ', data)
-    // });
+    document.addEventListener('setCurrentItem', data => {
+      self.setState({id : data.detail.id}) 
+    });
+    self.getItem()
   }
 
 
-
-  // setCurrentItem (item) {
-  //   // create a detail object to pass to your event with
-  //   // your data as the value
-  //   const detail = { detail: item }
-    
-  //   // create a custom event with an agreed-upon event
-  //   // name (in this case, 'setCurrentItem')
-  //     const event = new CustomEvent('setCurrentItem', detail);
-    
-  //   // add an event dispatcher to the document object, so
-  //   // others can 'listen' for this event no matter who
-  //   // dispatches it
-  //     document.dispatchEvent(event);
-  // }
-
-
-  // componentDidMount () {
-  //   // create a callback that executes some behavior when
-  //   // the event fires
-  //   const callback = (data) => this.setState({ foo: data });
-    
-  //   // create an event listener that matches the event
-  //   // name you're listening for on the document
-  //     document.addEventListener('setCurrentItem', callback);
-  // }
+  getItem() {
+    axios.get(`http://ec2-52-15-94-164.us-east-2.compute.amazonaws.com:3004/id/${this.state.id}`)
+    .then(function (response) {
+    this.setState({ reviews: response.data})
+  })
+    .catch(function (error) {
+    console.log(error)
+  })
+  }
 
 
 
