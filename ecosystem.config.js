0cee63dfa31f75a15b3e90e635247040db35eb2e
Miles Zander <mileszander@Miless-MacBook-Pro.local>
2019-07-20 12:06:49 -0500

    production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         ref: 'origin/master',
         repo: 'https://github.com/seabay-hratx42-fec/reviews.git',
         path: '/home/ubuntu/server/review',
-        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        'post-deploy': 'npm install && npx webpack --config webpack.config.js && pm2 startOrRestart ecosystem.config.js'
       }
     }
   }
